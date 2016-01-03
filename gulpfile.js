8fb83db75744ea22b897018fbecc1a09e82301b5
jchck <hey@justinchick.com>
2016-01-02 22:38:37 -0500

    install gulp-watch & browser sync and create gulp tasks for them

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var gulp 			= require('gulp'),
 	mqpacker 		= require('css-mqpacker'),
 	cssnano 		= require('cssnano'),
 	size			= require('gulp-size')
+	watch			= require('gulp-watch'),
+	browserSync 	= require('browser-sync').create(),
+	browserReload	= browserSync.reload;
 
 
 // This is the Gulp task named 'css' which executes the following function
@@ -34,4 +37,34 @@ gulp.task('css', function(){
 		.pipe(size({gzip: true, showFiles: true, title:'Processed & gZipped!'}))
 		// This is where the processed files get piped to ('./dest')
 		.pipe(gulp.dest('./dest'));
+});
+
+// Init browser-sync which starts a static server & allows for browsers to reload on filesave
+gulp.task('browser-sync', function(){
+	browserSync.init(null, {
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+// Call for reloading browsers
+gulp.task('bs-reload', function(){
+	 browserSync.reload();
+});
+
+/**
+ *
+ * The default task
+ * 
+ * + process CSS
+ * + starts a server at http://localhost:3000
+ * + reloads browser when change is made to css or HTML file
+ *
+ */
+
+gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
+	gulp.start(['css'], 'bs-reload');
+	gulp.watch('src/*', ['css']);
+	gulp.watch(['*.html', './**/*.html'], ['bs-reload']);
 });
