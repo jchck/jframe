5148be7f7b751929587634671ae54a559a662e5b
jchck <hey@justinchick.com>
2019-02-10 21:08:01 +0000

    renaming

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function (eleventy) {
-  eleventy.addLayoutAlias( 'base', 'layouts/base.liquid');
+  // eleventy.addLayoutAlias( 'base', 'layouts/base.liquid');
+  eleventy.addLayoutAlias( 'default', 'layouts/default.liquid' );
   return {
     dir: {
       inpit: 'src/views',
-      output: 'dist'
+      includes: '_includes',
+      output: 'dist',
+      passthroughFileCopy: true
     },
-    passthroughFileCopy: true
   }
 };
