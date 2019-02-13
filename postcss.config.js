de70a6729913683c92f7dadce63c277a31492614
jchck <hey@justinchick.com>
2019-02-13 02:32:10 +0000

    add and config cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
 const presetEnv = require('postcss-preset-env');
+const cssNano = require('cssnano');
 
 module.exports = {
   plugins: [
     presetEnv({
       stage: 1, // 0 (experimental) to 4 (stable)
+    }),
+    cssNano({
+      preset: 'default',
+      // or to pass options to modules ...
+      // preset: ['default', {
+      //   discardComments: {
+      //     removeAll: true
+      //   }
+      // }]
     })
   ]
 };
