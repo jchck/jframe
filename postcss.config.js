1dc3adaee8deb22c6d66f318883594a3b5bf2af9
jchck <hey@justinchick.com>
2019-02-13 02:53:42 +0000

    install & use css import

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 const presetEnv = require('postcss-preset-env');
 const cssNano = require('cssnano');
-
+const iimport = require('postcss-import');
 module.exports = {
   plugins: [
+    iimport(),
     presetEnv({
       stage: 1, // 0 (experimental) to 4 (stable)
     }),
