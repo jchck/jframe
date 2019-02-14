4f9c9627f57f274121cc67ed668eaab9362add4f
jchck <hey@justinchick.com>
2019-02-14 01:24:54 +0000

    bundle our css

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,23 @@
 const presetEnv = require('postcss-preset-env');
-const cssNano = require('cssnano');
-const iimport = require('postcss-import');
-const url = require('postcss-url');
+const nano = require('cssnano');
+const atImport = require('postcss-import');
+// const url = require('postcss-url');
 
 module.exports = {
   plugins: [
-    iimport(),
-    url({
-      url: 'copy',
-      // base path to search assets from
-      basePath: path.resolve('node_modules/bootstrap'),
-      // dir to copy assets
-      assetsPath: 'img',
-      useHash: true
-    }),
+    atImport(),
+    // url({
+    //   url: 'copy',
+    //   // base path to search assets from
+    //   basePath: path.resolve('node_modules/bootstrap'),
+    //   // dir to copy assets
+    //   assetsPath: 'img',
+    //   useHash: true
+    // }),
     presetEnv({
       stage: 1, // 0 (experimental) to 4 (stable)
     }),
-    cssNano({
+    nano({
       preset: 'default',
       // or to pass options to modules ...
       // preset: ['default', {
