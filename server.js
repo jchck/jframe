cee61a1f0827911bf6d9cb3ac4b2844d5e5c50ac
jchck <hey@justinchick.com>
2019-02-14 01:05:45 +0000

    comments

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const pkg = require('./package.json');
 const bs = require('browser-sync').create(pkg.name);
 
+/**
+ * Watch the src/views directory and on
+ * change, call eleventy & reload the browser
+ * @see https://www.browsersync.io/docs/api#api-watch
+ */
 bs.watch('./src/views/**/*', (event, file) => {
   if(event=='change') {
     require('@11ty/eleventy/cmd');
@@ -8,6 +13,10 @@ bs.watch('./src/views/**/*', (event, file) => {
   };
 });
 
+/**
+ * Start the server with these configs
+ * @see https://www.browsersync.io/docs
+ */
 bs.init({
   server: 'dist/',
   port: 8008,
